5035c0a91fedcbd6c8b4bb5efa7a849c84b5a6a1
markroskaric <mark.roskaric@hotmail.com>
2026-09-13 17:14:03 +0200

    1768_Merge_Strings_Alternately

--- LeetCode75/src/1768_merge_strings_alternately/solution.ts
+++ LeetCode75/src/1768_merge_strings_alternately/solution.ts
@@ -1,5 +1,19 @@
-function main():void {
-console.log("works");
-}
+function mergeAlternately(word1: string, word2: string): string {
 
-main();
+
+    let result: string = "";
+    let i: number = 0;
+    while (i < word1.length && i < word2.length) {
+        result += word1[i] + word2[i];
+        i++;
+    }
+    if (word1.length > i) {
+        return result += word1.slice(i);
+    } else if (word2.length > i) {
+        return result += word2.slice(i);
+    }
+
+    return result;
+}
+console.log(mergeAlternately("abc", "pqr"))
+
